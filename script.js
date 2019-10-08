017266b4fe68e00e138548b9ce2ea1533f221ef0
yuexinw6057 <56265160+yuexinw6057@users.noreply.github.com>
2019-10-08 11:03:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Kayla Weng"
+let userName = "yuexinw6057";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
